ab96aef2893f2c4f6cb070848d89307dada1194a
cherryApp <j.cserko@gmail.com>
2025-04-16 17:57:46 +0200

    get data from server

--- fetch-table/scripts/db.js
+++ fetch-table/scripts/db.js
@@ -4,3 +4,10 @@ export const getAll = (entity = 'products') => {
     return fetch(`${apiUrl}${entity}`)
         .then( response => response.json() );
 };
+
+export const remove = (id = 0, entity = 'products') => {
+    return fetch(`${apiUrl}${entity}/${id}`, {
+        method: 'DELETE'
+    })
+    .then( response => response.json() );
+};
